a3ef4a439774060f9825180f89b4a8529248a530
theendercore <theendercore@gmail.com>
2022-05-13 16:17:50 +0300

    oh help

--- src/main/java/com/theendercore/WafVerify.java
+++ src/main/java/com/theendercore/WafVerify.java
@@ -7,7 +7,7 @@ public final class WafVerify extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
-        System.out.println("Waffles are better!");
+        System.out.println("Waffles are better! oh god help me" );
 
     }
 
